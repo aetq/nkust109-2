c6a92b8f9d60551d1c972b9c24aca6676527651c
zo4j4 <71924011+harry2415@users.noreply.github.com>
2021-03-20 19:28:16 +0800

    hw_data

---
+++ 0311/ConsoleApp1/mode/name.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1.mode
+{
+    public class Name
+    {
+        public string county { get; set; }
+        public string Site { get; set; }
+        public string PM25 { get; set; }
+        public string DataCreationDate { get; set; }
+    }
+}
